2482a3995064b883f83760e3e66b6dc3f849c5b4
Dreaming-Bee <bsgunaweera16@gmail.com>
2026-01-30 00:40:08 +0530

    upadted billing

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,14 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple instances of Prisma Client in development
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   // @ts-ignore
-  if (!global.prisma) {
-    // @ts-ignore
-    global.prisma = new PrismaClient({ log: ['query'] })
-  }
+  if (!global.prisma) global.prisma = new PrismaClient({ log: ['query'] })
   // @ts-ignore
   prisma = global.prisma
 }
 
-export { prisma }
+export { prisma } // ✅ named export
